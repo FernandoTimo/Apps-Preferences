103eda64057da91e4079bb568ad64282507be962
FernandoTimo <fertimo99@gmail.com>
2021-03-31 11:46:40 -0500

    GH cli FernandoTimo sh

---
+++ Bash/ghf.sh
@@ -0,0 +1,4 @@
+echo github.com: > ~/.config/gh/hosts.yml 
+echo '  'oauth_token: tokenValidoDeGitHub >> ~/.config/gh/hosts.yml 
+echo '  'user: FernandoTimo >> ~/.config/gh/hosts.yml
+echo '  'git_protocol: ssh >> ~/.config/gh/hosts.yml
